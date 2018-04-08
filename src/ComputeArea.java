0b8400b5a23f28a32c01de8e5a7a49ac25c705bf
LuJerry <907257307@qq.com>
2018-04-08 22:21:43 +0800

    练习

---
+++ src/ComputeArea.java
@@ -0,0 +1,8 @@
+//程序2-1：一个计算圆面积的程序
+public class ComputeArea {
+	public static void main(String[]args){
+		int r=10;
+		double area=3.14*r*r;
+		System.out.println(area);
+	}
+}
---
+++ src/HelloWorldApp.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * 程序1-1：向控制台输出字符串的应用程序
+ * @author jerryls
+ *
+ */
+public class HelloWorldApp {
+	/*main方法是构成Java应用程序执行不可缺少的部分
+	@param args一个String类型的数组
+	*/
+	public static void main(String[]args){
+		System.out.println("Hello World!");
+	}
+}
